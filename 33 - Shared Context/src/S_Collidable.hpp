92fe7858d226c1c96d0e3aded4913e1d7d530cab
Robert Wells <robert.wells@thatgamesguy.co.uk>
2018-08-28 16:25:53 +0100

    completed tutorial 33

---
+++ 33 - Shared Context/src/S_Collidable.hpp
@@ -0,0 +1,36 @@
+#ifndef S_Collidable_hpp
+#define S_Collidable_hpp
+
+#include <vector>
+#include <memory>
+#include <set>
+
+#include "Object.hpp"
+#include "Quadtree.hpp"
+#include "Bitmask.hpp"
+
+class S_Collidable
+{
+public:
+    S_Collidable();
+    
+    void Add(std::vector<std::shared_ptr<Object>>& objects);
+    void ProcessRemovals();
+    void UpdatePositions(std::vector<std::shared_ptr<Object>>& objects);
+    void Resolve();
+    
+    void Update();
+    
+private:
+
+    void ProcessCollisions(std::vector<std::shared_ptr<Object>>& first, std::vector<std::shared_ptr<Object>>& second);
+    
+    std::map<CollisionLayer, Bitmask> collisionLayers;
+    std::map<CollisionLayer, std::vector<std::shared_ptr<C_BoxCollider>>> collidables;
+    
+    Quadtree collisionTree;
+};
+
+
+
+#endif /* S_Collidable_hpp */
