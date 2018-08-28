d511f08e0a40bf9f20e2a82aa16e1304ffbb5c9a
Robert Wells <robert.wells@thatgamesguy.co.uk>
2018-08-28 15:35:09 +0100

    finished tutorial 32

---
+++ 32 - Spawning Projectiles/src/C_KeyboardMovement.cpp
@@ -0,0 +1,57 @@
+#include "C_KeyboardMovement.hpp"
+#include "Object.hpp"
+
+C_KeyboardMovement::C_KeyboardMovement(Object* owner) : Component(owner), moveSpeed(300.f) {}
+
+void C_KeyboardMovement::Awake()
+{
+    animation = owner->GetComponent<C_Animation>();
+    velocity = owner->GetComponent<C_Velocity>();
+}
+
+void C_KeyboardMovement::SetInput(Input* input)
+{
+    this->input = input;
+}
+
+void C_KeyboardMovement::SetMovementSpeed(float moveSpeed)
+{
+    this->moveSpeed = moveSpeed;
+}
+
+void C_KeyboardMovement::Update(float deltaTime)
+{
+    if(input == nullptr)
+    {
+        return;
+    }
+    
+    //TODO: keyboardmovement should not interact with animation component.
+    if(animation->GetAnimationState() == AnimationState::Projectile)
+    {
+        velocity->Set(0.f, 0.f);
+        return;
+    }
+    
+    float xMove = 0.f;
+    if(input->IsKeyPressed(Input::Key::Left))
+    {
+        xMove = -moveSpeed;
+    }
+    else if(input->IsKeyPressed(Input::Key::Right))
+    {
+        xMove = moveSpeed;
+    }
+    
+    float yMove = 0.f;
+    if(input->IsKeyPressed(Input::Key::Up))
+    {
+        yMove = -moveSpeed;
+    }
+    else if(input->IsKeyPressed(Input::Key::Down))
+    {
+        yMove = moveSpeed;
+    }
+    
+    velocity->Set(xMove, yMove);
+}
