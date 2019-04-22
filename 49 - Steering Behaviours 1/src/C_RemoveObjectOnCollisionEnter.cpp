f4d86fb8304152b1e2392dca9b6c5d398e961273
Robert Wells <robert.wells@thatgamesguy.co.uk>
2019-04-22 09:48:12 +0100

    removed old tutorial 49

--- 49 - Steering Behaviours 1/src/C_RemoveObjectOnCollisionEnter.cpp
+++
@@ -1,9 +0,0 @@
-#include "C_RemoveObjectOnCollisionEnter.hpp"
-#include "Object.hpp"
-
-C_RemoveObjectOnCollisionEnter::C_RemoveObjectOnCollisionEnter(Object* owner) : Component(owner) {}
-
-void C_RemoveObjectOnCollisionEnter::OnCollisionEnter(std::shared_ptr<C_BoxCollider> other)
-{
-    owner->QueueForRemoval();
-}
