c4c9b4eeb7935b9d21e445f4de15d94b618ab725
Robert Wells <r.wells@platomedia.tv>
2019-04-22 10:01:51 +0100

    Commented out deprecated function

--- Xcode Build/that game engine/that_game_engine/Raycast.cpp
+++ Xcode Build/that game engine/that_game_engine/Raycast.cpp
@@ -3,61 +3,61 @@
 Raycast::Raycast(Quadtree& collisions) : collisions(collisions) {}
 
 //TODO: using int rather than unsigned int as exclusion id will cause problems if large number of entities.
-RaycastResult Raycast::Cast(const sf::Vector2f& from, const sf::Vector2f& to, int exclusionID)
-{
-    // Build empty raycast result
-    RaycastResult result;
-    result.collision = nullptr;
-    result.layer = CollisionLayer::Default;
-    result.point = sf::Vector2f();
-    
-    // Check if from and to are the same, if true return empty result
-    if(from == to)
-    {
-        return result;
-    }
-    
-    // Create sf::rect that encompasses line
-    sf::FloatRect collisionArea = BuildRect(from, to);
-    
-    //Debug::DrawRect(collisionArea, sf::Color::Red);
-    
-    // Retrieve entities from collision system within the raycast area
-    std::vector<std::shared_ptr<C_BoxCollider>> entities = collisions.Search(collisionArea);
-    
-    // If there are no eneities in the zone return empty result
-    if(entities.size() == 0)
-    {
-        return result;
-    }
-    
-    // Create a stepped line from first to second point
-    std::vector<sf::Vector2f> linePoints = BuildLinePoints(from, to);
-    
-    // for each point in the line check if it intersects with every entity
-    for (auto& p : linePoints)
-    {
-        for (auto& e : entities)
-        {
-            // Check if entity has been excluded from raycast collision
-            if(exclusionID == e->owner->instanceID->Get())
-            {
-                continue;
-            }
-            
-            sf::FloatRect entityRect = e->GetCollidable();
-            if(entityRect.contains(p))
-            {
-                result.collision = e->owner;
-                result.layer = e->GetLayer();
-                result.point = p;
-                return result;
-            }
-        }
-    }
-    
-    return result;
-}
+//RaycastResult Raycast::Cast(const sf::Vector2f& from, const sf::Vector2f& to, int exclusionID)
+//{
+//    // Build empty raycast result
+//    RaycastResult result;
+//    result.collision = nullptr;
+//    result.layer = CollisionLayer::Default;
+//    result.point = sf::Vector2f();
+//    
+//    // Check if from and to are the same, if true return empty result
+//    if(from == to)
+//    {
+//        return result;
+//    }
+//    
+//    // Create sf::rect that encompasses line
+//    sf::FloatRect collisionArea = BuildRect(from, to);
+//    
+//    //Debug::DrawRect(collisionArea, sf::Color::Red);
+//    
+//    // Retrieve entities from collision system within the raycast area
+//    std::vector<std::shared_ptr<C_BoxCollider>> entities = collisions.Search(collisionArea);
+//    
+//    // If there are no eneities in the zone return empty result
+//    if(entities.size() == 0)
+//    {
+//        return result;
+//    }
+//    
+//    // Create a stepped line from first to second point
+//    std::vector<sf::Vector2f> linePoints = BuildLinePoints(from, to);
+//    
+//    // for each point in the line check if it intersects with every entity
+//    for (auto& p : linePoints)
+//    {
+//        for (auto& e : entities)
+//        {
+//            // Check if entity has been excluded from raycast collision
+//            if(exclusionID == e->owner->instanceID->Get())
+//            {
+//                continue;
+//            }
+//            
+//            sf::FloatRect entityRect = e->GetCollidable();
+//            if(entityRect.contains(p))
+//            {
+//                result.collision = e->owner;
+//                result.layer = e->GetLayer();
+//                result.point = p;
+//                return result;
+//            }
+//        }
+//    }
+//    
+//    return result;
+//}
 
 //TODO: this uses much of the code from the other Cast function, can we simplify?
 RaycastResult Raycast::Cast(const sf::Vector2f& from, const sf::Vector2f& to, CollisionLayer layer)
